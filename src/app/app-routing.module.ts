5f4b7953c9bcf220839e3e1746d72782d6e6f897
Thandeka97 <215706010@tut4life.ac.za>
2022-03-09 00:05:58 +0200

    admin login in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginUIComponent } from './login-ui/login-ui.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'loginForm',component:LoginFormComponent},
+  {path:'loginUI', component:LoginUIComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const myroute = {LoginFormComponent}
+export const androute = {LoginUIComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginUIComponent } from './login-ui/login-ui.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    LoginUIComponent, 
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
